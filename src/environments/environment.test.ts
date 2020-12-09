93db495e2f6e1c76378ff4846f4914734c688fdb
Rebekah Higgins <rebekahapelt@gmail.com>
2020-12-09 11:06:16 +1000

    turn off cognito

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -25,7 +25,7 @@ export const config: EnvironementModel = {
     aws_user_pools_id: 'ap-southeast-2_xwJzu6o5o',
     aws_user_pools_web_client_id: '1kjv0a3rm18od63enl28q9smj2',
     feature_toggle: {
-        cognito_login: true,
+        cognito_login: false,
         download_graphs: true,
         techniques: true,
         io: true,
